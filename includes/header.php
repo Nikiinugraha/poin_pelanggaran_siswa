e2620100e03cc6be94a84e972a607c04abda719a
Nikiinugraha <Komangn806@gmail.com>
2026-01-30 08:27:19 +0800

    Add. php

--- includes/header.php
+++ includes/header.php
@@ -5,13 +5,13 @@
         <h1 style="text-align:center;">Aplikasi Poin Pelanggaran Siswa</h1>
         <nav>
             <ul>
-                <li><a href="/indomaret/halaman/dashboard.php">Dashboard</a></li>
-                <li><a href="/indomaret/halaman/data_siswa/list.php">Data Siswa</a></li>
-                <li><a href="/indomaret/halaman/data_pelanggaran/list.php">Data Pelanggaran</a></li>
-                <li><a href="/indomaret/halaman/entri_pelanggaran/list.php">Entri Pelanggaran</a></li>
-                <li><a href="/indomaret/halaman/entri_pelanggaran/list.php">Cetak Surat</a></li>
-                <li><a href="/indomaret/halaman/entri_pelanggaran/list.php">Laporan</a></li>
-                <li><a href="/indomaret/halaman/entri_pelanggaran/list.php">Logout</a></li>
+                <li><a href="/poin_pelanggaran_siswa/pages/index.php">Dashboard</a></li>
+                <li><a href="/poin_pelanggaran_siswa/pages/siswa/list.php">Data Siswa</a></li>
+                <li><a href="/poin_pelanggaran_siswa/pages/data_pelanggaran/list.php">Data Pelanggaran</a></li>
+                <li><a href="/poin_pelanggaran_siswa/pages/entri_pelanggaran/list.php">Entri Pelanggaran</a></li>
+                <li><a href="/poin_pelanggaran_siswa/pages/entri_pelanggaran/list.php">Cetak Surat</a></li>
+                <li><a href="/poin_pelanggaran_siswa/pages/entri_pelanggaran/list.php">Laporan</a></li>
+                <li><a href="/poin_pelanggaran_siswa/pages/entri_pelanggaran/list.php">Logout</a></li>
             </ul>
         </nav>
     </header>
--- process/siswa_process.php
+++ process/siswa_process.php
@@ -3,13 +3,13 @@ define ('ROOTPATH', $_SERVER['DOCUMENT_ROOT'] . '/poin_pelanggaran_siswa');
 include ROOTPATH . "/config/config.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id = $_POST['id'];
+    $nis = $_POST['id'];
     $action = $_POST['action'];
     $name = $_POST['nama_siswa'];
     
     
     if ($action == 'add') {
-        $query = "INSERT INTO siswa (id, nama_siswa) VALUES ('$id', '$name')";
+        $query = "INSERT INTO siswa (nis, nama_siswa,jenis_kelamin, alamat, nama_ayah, nama_ibu, pekerjaan_ayah, pekerjaan_ibu, pekerjaan_wali, no_telp_ayah, no_telp_ibu, no_telp_wali, alamat_ayah, alamat_ibu, alamat_wali, id_kelas), VALUES ('$nis', '$name', '$jenis_kelamin', '$alamat', '$nama_ayah', '$nama_ibu', '$pekerjaan_ayah', '$pekerjaan_ibu', '$pekerjaan_wali', '$no_telp_ayah', '$no_telp_ibu', '$no_telp_wali', '$alamat_ayah', '$alamat_ibu', '$alamat_wali', '$id_kelas')";
         mysqli_query($conn, $query);
     } elseif ($action == 'edit') {
         $query = "UPDATE siswa SET nama_siswa='$name' WHERE id=$id";
